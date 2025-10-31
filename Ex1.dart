417e1f83be714aaa120a45b6e5227ff5ccdce8b7
laihong <laihongpleng@gmail.com>
2025-10-31 07:07:09 +0700

    Ex1

---
+++ Ex1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello my name is Laihong',
+            style: TextStyle(
+              color: Color.fromARGB(255, 54, 191, 16),
+              fontSize: 60,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
